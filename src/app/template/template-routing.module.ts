50edbaae0b98f21addfebd1c58b2cc6231df9054
iOznny <f000170014@gmail.com>
2021-04-11 13:51:30 -0500

    cambio

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicsComponent } from './basics/basics.component';
+import { DinamicsComponent } from './dinamics/dinamics.component';
+import { SwitchesComponent } from './switches/switches.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    children: [
+      { path: 'basics', component: BasicsComponent },
+      { path: 'dinamics', component: DinamicsComponent },
+      { path: 'switches', component: SwitchesComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'basics' }
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
