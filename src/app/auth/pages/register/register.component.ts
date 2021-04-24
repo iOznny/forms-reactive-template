fc2ca855fded791076080cfccc507a41b5a4a1cb
iOznny <f000170014@gmail.com>
2021-04-24 14:25:22 -0500

    c

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public regNamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  form: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.regNamePattern)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoInvalid(campo: string) {
+    return this.form.get(campo)?.invalid && this.form.get(campo)?.touched;
+  }
+
+  submit() {
+    console.log(this.form.value);
+    
+    this.form.markAllAsTouched();
   }
 
 }
